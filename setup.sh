bcd8826c2ee5769aefcef475317a939151f5c9e2
CJ Oster <cj@cjoster.com>
2023-05-09 20:57:34 -0500

    Adds more config

--- setup.sh
+++ setup.sh
@@ -14,3 +14,19 @@ cleanup() {
 [ -z "${DEBUG+x}" ] || { echo "\"DEBUG\" environment variable is present. Enabling debugging output."; set -x; }
 
 trap cleanup EXIT
+
+userdel -r ec2-user
+touch /etc/skel/.hushlogin
+
+useradd -m -u 1000 -g 1000 -G wheel lordvadr
+mkdir ~lordvadr/.ssh
+cat authorized_keys > ~lordvadr/.ssh/authorized_keys
+chown -R lordvadr.lordvadr ~lordvadr
+chmod 700 ~lordvadr/.ssh
+chmod 600 ~lordvadr/.ssh/authorized_keys
+
+echo "%wheel ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/wheel
+
+dnf -y update
+
+dnf -y install tmux
